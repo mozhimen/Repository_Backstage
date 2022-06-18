149203570b135c49926e74a71b6ac813f51a0845
mozhimen <mozhimen@163.com>
2022-06-18 10:09:22 +0800

    0618

---
+++ PublicBackStage/src/main/java/com/mozhimen/pbackstage/PublicBackStageApplication.java
@@ -0,0 +1,13 @@
+package com.mozhimen.pbackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicBackStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicBackStageApplication.class, args);
+    }
+
+}
---
+++ PublicBackStage/src/test/java/com/mozhimen/pbackstage/PublicBackStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozhimen.pbackstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicBackStageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
